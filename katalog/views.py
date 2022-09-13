2a50bb5ad09796a905f7c319310d3e6af85833d7
amanda.nurul11 <amanda.nurul11@ui.ac.id>
2022-09-13 13:55:06 +0700

    Update models.py, views.py, urls.py, and katalog.html

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+        'nama' : 'Amanda Nurul Izzah',
+        'katalog' : data_katalog
+    }
+
+    return render(request, "katalog.html", context)
